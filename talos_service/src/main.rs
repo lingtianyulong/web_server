c0bc65b52d18c8fae706cd0a18dce818c3e6bf05
lingtianyulong <lingtianyulong@126.com>
2025-08-06 20:56:19 +0800

    1. 新增日志模块, 并添加从配置文件中读取日志参数的功能;

--- talos_service/src/main.rs
+++ talos_service/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, web};
+mod logger;
+
+async fn index() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if !logger::init("confgs/logger_config.json") {
+        eprintln!("Failed to initialize logger");
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to initialize logger",
+        ));
+    }
+
+    logger::info("Starting server");
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(index))
+        // .service(greet)
+        // .service(create_user)
+        // .service(user_exist)
+        // .service(login)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
+
+    logger::info("Server started");
+    Ok(())
 }
