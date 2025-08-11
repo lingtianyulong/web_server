ab177b236dc0f22571487ca6544308b0fbec7143
lingtianyulong <lingtianyulong@126.com>
2025-08-11 16:16:34 +0800

    1. 添加用户登录和忘记密码接口;

--- talos_service/src/routes/user_route.rs
+++ talos_service/src/routes/user_route.rs
@@ -7,3 +7,17 @@ async fn register_user(user: web::Json<UserResponse>) -> impl Responder {
     let new_user = User::create_user_from_response(user.into_inner());
     HttpResponse::Ok().body(new_user.to_json())
 }
+
+// 用户登录
+#[post("/login")]
+async fn login_user(user: web::Json<UserResponse>) -> impl Responder {
+    let new_user = User::create_user_from_response(user.into_inner());
+    HttpResponse::Ok().body(new_user.to_json())
+}
+
+// 重置密码
+#[post("/reset_password")]
+async fn reset_password(user: web::Json<UserResponse>) -> impl Responder {
+    let new_user = User::create_user_from_response(user.into_inner());
+    HttpResponse::Ok().body(new_user.to_json())
+}
